7dde19629c9827bfcc82665ff3df79aee9195fa4
GabigolDoSenai <leticiag.soares.dev@gmail.com>
2024-05-10 12:22:47 -0300

    first commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import Styles from './Styles'
+
+function App() {
+
+  return (
+    <>
+    <Div>
+      <Titulo >Hello World!</Titulo>
+    </Div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Styles.js
@@ -0,0 +1,21 @@
+export const  Cores = {
+    branco: '#fff',
+    preto: '#000',
+    amarelo: '#e1ce1b',
+    rosa: '#f2d',
+    verde: '#1ddc17'
+  }
+  
+export const Titulo = styled.h1`
+    color: ${Cores.rosa};
+  `
+  
+  export const Div = styled.div`
+    width: 400px;
+    height: 200px;
+    background-color:${Cores.verde};
+    border-radius: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `
